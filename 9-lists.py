1c47b3e8e7df7e72a6751745a0b3034e43909068
shahan <shahan.ahmed@apify.ai>
2022-12-20 18:28:48 +0500

    till 9th chapter

---
+++ 9-lists.py
@@ -0,0 +1,8 @@
+friends = ['Kevin', 'Karen', 'Jim', 'Oscar', 'Toby']
+
+print(friends[0]) # get the first one
+print(friends[-1]) # get the last one
+print(friends[1:3]) # get the rage
+
+friends[1] = 'Shahan'
+print(friends[1])
