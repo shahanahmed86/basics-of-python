c43ed1bde828e0ccd3c38d53cfd7f9cac07a146f
shahan <shahan.ahmed@apify.ai>
2022-12-20 18:11:08 +0500

    till 8th chapter

---
+++ 8-mad-libs-game.py
@@ -0,0 +1,7 @@
+color = input('Enter a color: ')
+noun = input('Enter a plural noun: ')
+celebrity = input('Enter a celebrity: ')
+
+print(f'Roses are {color}')
+print(f'{noun} are blue')
+print(f'I love {celebrity}')
